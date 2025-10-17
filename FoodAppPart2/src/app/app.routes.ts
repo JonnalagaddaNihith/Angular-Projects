d3e30afc2eabe04a1c000b8cf909327c2aedb73c
Nihith303 <nihithjonnalagadda@gmail.com>
2025-10-17 16:07:20 +0530

    Learned Routing

---
+++ FoodAppPart2/src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ContactusComponent } from './components/contactus/contactus.component';
+import { MenuComponent } from './components/menu/menu.component';
+
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'contact', component: ContactusComponent },
+  { path: 'menu', component: MenuComponent },
+];
