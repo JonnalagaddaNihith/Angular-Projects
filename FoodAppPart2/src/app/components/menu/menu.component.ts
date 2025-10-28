877df5ef88242c8f8606910bda249ed46a48fa08
Nihith303 <nihithjonnalagadda@gmail.com>
2025-10-28 16:21:43 +0530

    More about Router

--- FoodAppPart2/src/app/components/menu/menu.component.ts
+++ FoodAppPart2/src/app/components/menu/menu.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import foodDataFromFile from '../../../../public/foodMenu.json';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './menu.component.html',
-  styleUrl: './menu.component.css'
+  styleUrl: './menu.component.css',
 })
 export class MenuComponent {
-
+  foodData: any = [];
+  constructor() {
+    this.foodData = foodDataFromFile;
+    console.log(this.foodData);
+  }
 }
