9ad81ec281f70559d09bdbbf993eb6705d999cd8
Nihith303 <nihithjonnalagadda@gmail.com>
2025-11-05 15:09:27 +0530

    added a custom gender pipe

--- src/app/gender.pipe.ts
+++ src/app/gender.pipe.ts
@@ -2,17 +2,14 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
   name: 'gender',
-  standalone: true
+  standalone: true,
 })
 export class GenderPipe implements PipeTransform {
   transform(name: string, ...args: string[]): string {
     if (!args[0]) return name;
     const gender = args[0].toLowerCase();
-    if (gender === 'male' || gender === 'm')
-      return 'Mr. ' + name;
-    else if (gender === 'female' || gender === 'f') 
-      return 'Ms. ' + name;
+    if (gender === 'male' || gender === 'm') return 'Mr. ' + name;
+    else if (gender === 'female' || gender === 'f') return 'Ms. ' + name;
     return name;
   }
-
 }
